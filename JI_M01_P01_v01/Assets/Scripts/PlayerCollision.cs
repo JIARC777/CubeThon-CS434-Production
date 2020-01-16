d1390331af614b7ca267fe8a0d8a43eaa9d40295
Jeremy Ivan <59781543+JeremyI@users.noreply.github.com>
2020-01-15 20:27:45 -0700

    Update and Build

---
+++ JI_M01_P01_v01/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+	// Grab a reference to the movement script
+	public GameObject[] obstacles;
+	public PlayerMovement movement;
+	public Material illuminateMat;
+	public Material stdMat;
+	// Set up listener for collision
+	void OnCollisionEnter(Collision collInstnace)
+	{
+		if (collInstnace.collider.tag == "Obstacle" || collInstnace.collider.tag == "Seeker")
+		{
+			// Deactivate movement script upon impact
+			Debug.Log("Hit Obstacle");
+			movement.enabled = false;
+			FindObjectOfType<GameManager>().EndGame();
+		}
+	}
+}
---
+++ JI_M01_P01_v01/Assets/Scripts/Powerup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+	// References to the two materials the obstacles will have
+	public Material illuminateMat;
+	public Material stdMat;
+	// Reference for all of the obstacles
+	private GameObject[] obstacles;
+	private void OnTriggerEnter(Collider other)
+	{
+		StartCoroutine(ResetMaterial());
+	}
+
+	// Run as a couroutine so timing can be applied between setting and resetting the material
+	IEnumerator ResetMaterial()
+	{
+		// find all objects tagged as an obstacle
+		obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+		foreach (GameObject obstacle in obstacles)
+		{
+			// For each obstacle found replace the material
+			obstacle.GetComponent<MeshRenderer>().material = illuminateMat;
+		}
+		// wait for five seconds and reset material back 
+		yield return new WaitForSeconds(5);
+		foreach (GameObject obstacle in obstacles)
+		{
+			obstacle.GetComponent<MeshRenderer>().material = stdMat;
+		}
+	}
+}
+
